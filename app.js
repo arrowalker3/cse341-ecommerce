a4e3cd30585579feac1e2943126e6a113974f470
arrowalker3 <arrowalker3@gmail.com>
2021-09-25 19:38:50 -0600

    Added port switching between development and heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
 
@@ -27,4 +28,4 @@ app.use((req, res, next) => {
     res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
 });
 
-app.listen(3000);
+app.listen(PORT);
